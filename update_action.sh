18e3adaf9558afaff72774b1c0554696d4c40e82
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2022-10-22 11:20:50 +0800

    修复QQ浏览器插件误杀

--- update_action.sh
+++ update_action.sh
@@ -1026,7 +1026,8 @@ cat << "key" > ${file}
 ^127.0.0.1[[:space:]]app.chat.xiaomi.net$
 #部分设备的小米云服务
 ^127.0.0.1[[:space:]]api.g.micloud.xiaomi.net$
-
+#QQ浏览器插件误杀
+^127.0.0.1[[:space:]]tbsone.imtt.qq.com$
 
 
 
