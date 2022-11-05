dfd2486fc1c2f9699c07c399c10d28c5ccb29820
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2022-11-06 01:49:54 +0800

    Update update_action.sh

--- update_action.sh
+++ update_action.sh
@@ -673,9 +673,70 @@ cat << "key" > ${file}
 
 
 #谷歌翻译
+108.177.122.90 translate.googleapis.com
+142.250.0.90 translate.googleapis.com
+142.250.10.90 translate.googleapis.com
+142.250.100.90 translate.googleapis.com
+142.250.101.90 translate.googleapis.com
+142.250.105.90 translate.googleapis.com
+142.250.107.90 translate.googleapis.com
+142.250.11.90 translate.googleapis.com
+142.250.110.90 translate.googleapis.com
+142.250.111.90 translate.googleapis.com
+142.250.112.90 translate.googleapis.com
+142.250.12.90 translate.googleapis.com
+142.250.125.90 translate.googleapis.com
+142.250.126.90 translate.googleapis.com
+142.250.128.90 translate.googleapis.com
+142.250.136.90 translate.googleapis.com
+142.250.185.174 translate.googleapis.com
+142.250.185.238 translate.googleapis.com
+142.250.189.206 translate.googleapis.com
+142.250.203.142 translate.googleapis.com
+142.250.218.14 translate.googleapis.com
+142.250.27.90 translate.googleapis.com
+142.250.28.90 translate.googleapis.com
+142.250.30.90 translate.googleapis.com
+142.250.31.90 translate.googleapis.com
+142.250.4.90 translate.googleapis.com
+142.250.8.90 translate.googleapis.com
+142.250.9.90 translate.googleapis.com
+142.250.96.90 translate.googleapis.com
+142.250.97.90 translate.googleapis.com
+142.250.98.90 translate.googleapis.com
+142.251.10.138 translate.googleapis.com
+142.251.116.101 translate.googleapis.com
+142.251.40.174 translate.googleapis.com
+142.251.5.90 translate.googleapis.com
+142.251.9.90 translate.googleapis.com
+172.217.0.46 translate.googleapis.com
+172.217.13.142 translate.googleapis.com
+172.217.16.46 translate.googleapis.com
+172.217.192.90 translate.googleapis.com
+172.217.195.90 translate.googleapis.com
+172.217.203.90 translate.googleapis.com
+172.217.204.90 translate.googleapis.com
+172.217.214.90 translate.googleapis.com
+172.217.215.90 translate.googleapis.com
+172.217.222.90 translate.googleapis.com
+172.217.31.142 translate.googleapis.com
+172.253.112.90 translate.googleapis.com
+172.253.113.90 translate.googleapis.com
+172.253.114.90 translate.googleapis.com
+172.253.115.90 translate.googleapis.com
+172.253.116.90 translate.googleapis.com
+172.253.122.90 translate.googleapis.com
+172.253.123.90 translate.googleapis.com
+172.253.124.90 translate.googleapis.com
+172.253.126.90 translate.googleapis.com
+172.253.62.90 translate.googleapis.com
 180.163.151.162 translate.googleapis.com
-203.208.40.66 translate.googleapis.com
 203.208.40.66 translate.google.com
+203.208.40.66 translate.googleapis.com
+216.58.209.174 translate.googleapis.com
+216.58.214.14 translate.googleapis.com
+216.58.220.142 translate.googleapis.com
+
 
 #22.11.05
 #coolapk
