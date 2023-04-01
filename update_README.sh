9f434ca78e73ca430567a5a344bfa26406035b2a
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2023-04-02 01:40:30 +0800

    Update update_README.sh

--- update_README.sh
+++ update_README.sh
@@ -67,6 +67,7 @@ cat << key > `pwd`/README.md
 完整版累积拦截域名: ${count_all}
 保留广告奖励累积拦截域名: ${count_reward}
 \`\`\`
+## 访问人数 : ![visitors](https://visitor-badge.glitch.me/badge?page_id=lingeringsound.10007_auto&left_color=green&right_color=red)
 
 ## 订阅
 ### 国内请用 **镜像链接**  
@@ -91,6 +92,7 @@ cat << key > `pwd`/README_en.md
 all: ${count_all}
 reward: ${count_reward}
 \`\`\`
+### ![visitors](https://visitor-badge.glitch.me/badge?page_id=lingeringsound.10007&left_color=green&right_color=red)
 
 | **name** | **links** |
 | :-- | :-- |
@@ -118,6 +120,7 @@ cat << key > `pwd`/README.md
 完整版累积拦截域名: $count_all
 保留广告奖励累积拦截域名: $count_reward
 \`\`\`
+
 
 | **名称** | **链接** | **镜像链接** |
 | :-- | :-- | :-- |
