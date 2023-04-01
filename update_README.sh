67a575079202947598b222642c46edbd51d07567
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2023-04-01 13:22:00 +0800

    Update update_README.sh

--- update_README.sh
+++ update_README.sh
@@ -71,11 +71,11 @@ cat << key > `pwd`/README.md
 ## 订阅
 ### 国内请用 **镜像链接**  
 
-| **名称** | **链接** | **镜像链接** |
-| :-- | :-- | :-- |
-| **完整版** | [订阅链接](https://raw.githubusercontent.com/lingeringsound/10007_auto/master/all) | [Gitcode镜像](https://gitcode.net/weixin_45617236/10007_auto/-/raw/master/all) |
-| **保留广告奖励** | [订阅链接](https://raw.githubusercontent.com/lingeringsound/10007_auto/master/reward) | [Gitcode镜像](https://gitcode.net/weixin_45617236/10007_auto/-/raw/master/reward) |
-| **Adblock规则** | [订阅链接](https://raw.githubusercontent.com/lingeringsound/10007_auto/master/adb.txt) | [Gitcode镜像](https://gitcode.net/weixin_45617236/10007_auto/-/raw/master/adb.txt) |
+| **名称** | **链接** | **镜像链接** | **Github订阅加速链接** |
+| :-- | :-- | :-- | :-- |
+| **完整版** | [订阅链接](https://raw.githubusercontent.com/lingeringsound/10007_auto/master/all) | [Gitcode镜像](https://gitcode.net/weixin_45617236/10007_auto/-/raw/master/all) | [加速链接](https://raw.gitmirror.com/lingeringsound/10007_auto/master/all) |
+| **保留广告奖励** | [订阅链接](https://raw.githubusercontent.com/lingeringsound/10007_auto/master/reward) | [Gitcode镜像](https://gitcode.net/weixin_45617236/10007_auto/-/raw/master/reward) | [加速链接](https://raw.gitmirror.com/lingeringsound/10007_auto/master/reward) |
+| **Adblock规则** | [订阅链接](https://raw.githubusercontent.com/lingeringsound/10007_auto/master/adb.txt) | [Gitcode镜像](https://gitcode.net/weixin_45617236/10007_auto/-/raw/master/adb.txt) | [加速链接](https://raw.gitmirror.com/lingeringsound/10007_auto/master/adb.txt) |
 
 
 ## **[捐赠地址](https://github.com/lingeringsound/10007)**
